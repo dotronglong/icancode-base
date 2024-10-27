ae506b0524d4b57153e12a296bef7913ede02e2d
Long Do <dotronglong@live.com>
2024-10-27 19:47:29 +0700

    Remove unnecessary arguments of HashMapConvertible

--- lib/HashMap.ts
+++ lib/HashMap.ts
@@ -3,11 +3,9 @@ import {HashMap} from './Types';
 export interface HashMapConvertible {
   /**
    * Convert to HashMap
-   * @param {string[]=} only Result contains only these fields
-   * @param {string[]=} ignore Result will ignore these fields
    * @return {HashMap}
    */
-  toHashMap(only?: string[], ignore?: string[]): HashMap;
+  toHashMap(): HashMap;
 }
 
 export interface HashMapImportable {
