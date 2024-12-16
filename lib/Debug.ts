425c358773a08630bcc315bc624fd36921960640
Do Trong Long <dotronglong@live.com>
2024-12-16 18:34:53 +0700

    Improve debug (#14)

--- lib/Debug.ts
+++ lib/Debug.ts
@@ -1,24 +1,22 @@
 type DebugFunction = (...args: unknown[]) => void;
+const emptyDebugFunction: DebugFunction = () => {};
 
 /**
- * Creates a namespaced debug function.
+ * Creates a debug function to log messages.
  * @param {string} module - The module name to include in the debug namespace.
- * @param {string} [namespace='icancode'] - The global namespace.
  * @return {DebugFunction} A debug function for logging.
  */
-export const debug = (module: string, namespace: string = 'icancode'): DebugFunction => {
-  let debugFunction: DebugFunction = () => {};
+export const createDebug = (module: string): DebugFunction => {
+  let debugFunction: DebugFunction = emptyDebugFunction;
 
   if (process.env.DEBUG) {
-    const name = `${namespace}:${module}`;
     try {
-      // Dynamically require 'debug' only when needed
-      const createDebug = require('debug');
-      debugFunction = createDebug(name);
+      // Directly call require('debug') with the module name
+      debugFunction = require('debug')(module);
     } catch (error) {
       console.warn(`Failed to load 'debug' module. Using console fallback.`, error);
       debugFunction = (...args: unknown[]): void => {
-        console.log(`[${name}]`, ...args);
+        console.log(`[${module}]`, ...args);
       };
     }
   }
